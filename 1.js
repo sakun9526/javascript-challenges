09d3ae006c9f1bb26be2b6bcfd9d72b3d6f47433
sakun9526 <sakunrathnayaka@yahoo.com>
2022-10-13 11:15:47 +0530

    fundamentals 1 done

--- 1.js
+++ 1.js
@@ -5,7 +5,31 @@
 Current time is : 10 PM : 30 : 38
 
 */
+
+//Regular answer 
+
 let dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let today = new Date().getDay();
 
 console.log('Today is: ',dayOfWeek[today])
+
+let hour = new Date().getHours()
+let minutes = new Date().getMinutes()
+let seconds = new Date().getSeconds()
+let ampm = hour >= 12 ? 'pm' : 'am';
+
+console.log('Time is: ', `${hour}${ampm} : ${minutes} : ${seconds}`)
+
+//improved answer
+const getDateTime = (date) => {
+    const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const today = date.getDay();
+    const hour = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    const ampm = hour >= 12 ? 'pm' : 'am';
+
+    console.log(`Today is: ${dayOfWeek[today]} and Time is: ${hour}${ampm} : ${minutes} : ${seconds}`)
+}
+
+getDateTime(new Date())
